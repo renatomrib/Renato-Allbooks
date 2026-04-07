1b32a840ff67fa3020ac81f99b9fbb06a1593814
renatomrib <renatomrib@gmail.com>
2026-04-07 09:33:41 -0300

    Altera o server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("Acesse esta API em http://localhost:8000")
+  console.log("Boas vindas a API do Allbooks")
+  console.log("API disponivel atraves da url http://localhost:8000")
 })
